a4e0f07f52c0f1697416c94aa13e34fd458accac
Anton <pro.grishin@gmail.com>
2021-04-02 18:29:11 +0300

    News App v 0.2

--- app.js
+++ app.js
@@ -50,69 +50,43 @@ const newService = (function () {
 })();
 
 function renderNews(newsArr) {
-  let body = document.querySelector('#body');
-  let fragment = document.createDocumentFragment();
-  for (let i = 0; i < newsArr.length; i++) {
-    if (i % 2 !== 0) {
-      continue;
-    }
-    else {
-      // Create ROW
-      let row = document.createElement('div');
-      row.classList.add('row');
-      console.log('row', i);
-      for (let j = i; j < i + 2; j++) {
-        // Create two COL
-        console.log('col', j);
-        let col = document.createElement('div');
-        let card = document.createElement('div');
-        let cardImage = document.createElement('div');
-        let img = document.createElement('img');
-        let span = document.createElement('span');
+  let body = document.querySelector('#q');
+  let fragment = '';
 
-        col.classList.add('col', 's6');
-        card.classList.add('card');
-        cardImage.classList.add('card-image');
-        img.src = newsArr[j].urlToImage;
-        span.classList.add('card-title');
-        span.textContent = newsArr[j].title;
-        span.style.backgroundColor = 'black';
-        span.style.opacity = '0.7';
-        span.style.fontSize = '15px';
-        span.style.width = '100%';
-
-        row.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardImage);
-        cardImage.appendChild(img);
-        cardImage.appendChild(span);
-        fragment.appendChild(row);
-        body.appendChild(fragment);
-      }
-    };
+  for (el of newsArr) {
+    let ht = createCard(el.urlToImage, el.title);
+    fragment += ht;
   }
+  console.log(fragment);
+  body.insertAdjacentHTML('afterbegin', fragment);
+}
 
 
-
-
-
-
-
+function createCard(img, title) {
+  return `
+      <div class="col s12">
+        <div class="card">
+        <div class="card-image">
+         <img src="${img}">
+        <span>${title}</span>
+        </div>
+        </div>
+      </div>
+    `;
 }
 
 
 function loadNews() {
-  // newService.topHeadlines('ru', onGetResponse);
-  let search = document.querySelector('#search');
-  search.addEventListener('change', e => {
-    let query = e.target.value;
-    newService.everything(query, onGetResponse);
-  });
+  newService.topHeadlines('us', onGetResponse);
+  // let search = document.querySelector('#search');
+  // search.addEventListener('change', e => {
+  //   let query = e.target.value;
+  //   newService.everything(query, onGetResponse);
+  // });
 
 }
 
 function onGetResponse(err, res) {
   renderNews(res.articles);
-  
 }
 
